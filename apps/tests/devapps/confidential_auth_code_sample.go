db481dc461f0a45e143c60336f8edaa6d0fbb83d
mr-oliva <kutsuzawa2851@gmail.com>
2023-03-15 23:48:58 +0900

    add confidential_auth_code sample

--- apps/tests/devapps/confidential_auth_code_sample.go
+++ apps/tests/devapps/confidential_auth_code_sample.go
@@ -3,21 +3,39 @@
 
 package main
 
-/*
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
+)
+
 var (
 	accessToken        string
 	confidentialConfig = CreateConfig("confidential_config.json")
 	app                confidential.Client
+	account            confidential.Account
 )
 
 // TODO(msal): I'm not sure what to do here with the CodeChallenge and State. authCodeURLParams
 // is no more.  CodeChallenge is only used now in a confidential.AcquireTokenByAuthCode(), which
 // this is not using.  Maybe now this is a two step process????
 func redirectToURLConfidential(w http.ResponseWriter, r *http.Request) {
-	// Getting the URL to redirect to acquire the authorization code
-	authCodeURLParams.CodeChallenge = confidentialConfig.CodeChallenge
-	authCodeURLParams.State = confidentialConfig.State
-	authURL, err := app.AuthCodeURL(context.Background(), confidentialConfig.ClientID, confidentialConfig.RedirectURI, confidentialConfig.Scopes)
+	result, err := app.AcquireTokenSilent(r.Context(), confidentialConfig.Scopes, confidential.WithSilentAccount(account))
+	if err == nil {
+		accessToken = result.AccessToken
+		fmt.Fprintln(w, "[Non-Interactive] Access token is "+accessToken)
+		return
+	}
+	if err.Error() != "no token found" {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	authURL, err := app.AuthCodeURL(r.Context(), confidentialConfig.ClientID, confidentialConfig.RedirectURI, confidentialConfig.Scopes)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
@@ -28,76 +46,47 @@ func redirectToURLConfidential(w http.ResponseWriter, r *http.Request) {
 }
 
 func getTokenConfidential(w http.ResponseWriter, r *http.Request) {
-	// Getting the authorization code from the URL's query
-	states, ok := r.URL.Query()["state"]
-	if !ok || len(states[0]) < 1 {
-		log.Fatal(errors.New("State parameter missing, can't verify authorization code"))
-	}
 	codes, ok := r.URL.Query()["code"]
 	if !ok || len(codes[0]) < 1 {
+		fmt.Fprintln(w, errors.New("Authorization code missing"))
 		log.Fatal(errors.New("Authorization code missing"))
-	}
-	if states[0] != config.State {
-		log.Fatal(errors.New("State parameter is incorrect"))
+		return
 	}
 	code := codes[0]
 	// Getting the access token using the authorization code
 	result, err := app.AcquireTokenByAuthCode(
 		context.Background(),
+		code,
+		confidentialConfig.RedirectURI,
 		confidentialConfig.Scopes,
-		confidential.CodeChallenge(code, confidentialConfig.CodeChallenge),
 	)
 	if err != nil {
+		fmt.Fprintln(w, err.Error())
 		log.Fatal(err)
+		return
 	}
+	account = result.Account
 	// Prints the access token on the webpage
-	fmt.Fprintf(w, "Access token is "+result.GetAccessToken())
-	accessToken = result.GetAccessToken()
+	accessToken = result.AccessToken
+	fmt.Fprintln(w, "[Interactive] Access token is "+accessToken)
 }
 
-// TODO(msal): Needs to use an x509 certificate like the other now that we are not using a
-// thumbprint directly.
-/*
-func acquireByAuthorizationCodeConfidential(ctx context.Context) {
-	key, err := os.ReadFile(confidentialConfig.KeyFile)
+func acquireByAuthorizationCodeConfidential() {
+	cred, err := confidential.NewCredFromSecret(confidentialConfig.ClientSecret)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	certificate, err := msal.CreateClientCredentialFromCertificate(confidentialConfig.Thumbprint, key)
+	options := confidential.WithCache(cacheAccessor)
+	app, err = confidential.New(confidentialConfig.Authority, confidentialConfig.ClientID, cred, options)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	options := msal.DefaultConfidentialClientApplicationOptions()
-	options.Accessor = cacheAccessor
-	options.Authority = confidentialConfig.Authority
-	app, err := msal.NewConfidentialClientApplication(confidentialConfig.ClientID, certificate, &options)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var userAccount shared.Account
-	for _, account := range app.Accounts(ctx) {
-		if account.PreferredUsername == confidentialConfig.Username {
-			userAccount = account
-		}
-	}
-	result, err := app.AcquireTokenSilent(
-		context.Background(),
-		confidentialConfig.Scopes,
-		&msal.AcquireTokenSilentOptions{
-			Account: userAccount,
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Access token is " + result.GetAccessToken())
-	accessToken = result.GetAccessToken()
 
 	http.HandleFunc("/", redirectToURLConfidential)
 	// The redirect uri set in our app's registration is http://localhost:port/redirect
 	http.HandleFunc("/redirect", getTokenConfidential)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	port := "8080"
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
-*/
